4f220b104f555c12edbd222d13192609935e20e0
biaks <ianiskr@gmail.com>
2021-07-14 23:52:00 +0300

    Fix bug with shutdown SaveToFile thread and bug in simpleAPI

--- simpleapi/src/simpleapi.cpp
+++ simpleapi/src/simpleapi.cpp
@@ -3,11 +3,14 @@
 #include "prometheus/registry.h"
 #include "prometheus/save_to_file.h"
 
+#include <memory>
+
 namespace prometheus {
   namespace simpleapi {
 
-    Registry registry;
-    SaveToFile saver(registry, std::chrono::seconds(5), std::string("./metrics.prom"));
+    std::shared_ptr<Registry> registry_ptr = std::make_shared<Registry>();
+    Registry&                 registry = *registry_ptr;
+    SaveToFile saver(registry_ptr, std::chrono::seconds(5), std::string("./metrics.prom"));
 
   }
 }
