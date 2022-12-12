76be2bc7874bedd257c48340666d5c2305ac0912
aSemy <897017+aSemy@users.noreply.github.com>
2022-12-12 22:10:33 +0100

    Update histogram.h

--- core/include/prometheus/histogram.h
+++ core/include/prometheus/histogram.h
@@ -105,7 +105,7 @@ namespace prometheus {
         auto cumulative_count = 0ULL;
         metric.histogram.bucket.reserve(bucket_counts_.size());
         for (std::size_t i{0}; i < bucket_counts_.size(); ++i) {
-          cumulative_count += static_cast<std::size_t>(bucket_counts_[i].Value());
+          cumulative_count += static_cast<std::size_t>(bucket_counts_[i].Get());
           auto bucket = ClientMetric::Bucket{};
           bucket.cumulative_count = cumulative_count;
           bucket.upper_bound = (i == bucket_boundaries_.size()
